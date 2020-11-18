1a55018ce3d3868250a2bb427ac44d443717c367
Sergey-dotcom <70769015+Sergey-dotcom@users.noreply.github.com>
2020-11-18 19:15:16 +0300

    Add files via upload
    unit: CSS_Variables_and_JS task

---
+++ CSS_Variables_and_JS/script.js
@@ -0,0 +1,12 @@
+var inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => input.addEventListener("change", inputUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", inputUpdate));
+
+function inputUpdate() {
+  var suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
